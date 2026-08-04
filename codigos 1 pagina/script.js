f63a6c8e450e2f25146fd0c804660f4e78751b72
juliokubiack <julio.kubiack@estudantes.ifc.edu.br>
2026-08-04 14:23:23 -0300

    commit

---
+++ codigos 1 pagina/script.js
@@ -0,0 +1,14 @@
+let objetivo=0;
+let dias=0;
+let entrar=prompt("você deseja continuar?").toUpperCase();
+if(entrar=="SIM"){
+    dias=+prompt("quantos dias você pode dias ativos na semana vocÊ pode estar contribuindo para o proceguimento de nossos objetivos ")
+    do{
+        objetivo=+prompt("qual é o seu objetivo dentro de nosso...").toUpperCase()
+
+    }
+    while(dias<0 || objetivo != "EMAGRECIMENTO" || objetivo != "GANHO MASSA")
+}
+else{
+    Sair...
+}
